76dc432122bd523779e45b069a7b65666251a875
Umar-Raza <chintoraza279@gmail.com>
2025-04-06 23:34:37 +0500

    set loading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,12 +7,15 @@ import { SearchInput } from '../components/searchInput/SearchInput';
 import { GoogleGenAI } from "@google/genai";
 import { IoSend } from "react-icons/io5";
 import { Navbar } from '../components/navbar/Navbar';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export const Home = () => {
 
     const [isResponsScreen, setIsResponsScreen] = useState(false)
     const [question, setQuestion] = useState("")
     const [allMassages, setAllMassages] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
     const handleRequest = () => {
         if (question) {
             generateResponse(question)
@@ -21,9 +24,11 @@ export const Home = () => {
         }
     }
     const generateResponse = (msg) => {
+       setIsLoading(true)
         let allMassages = []
         const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_APP_GEMkEY });
         async function main() {
+
             const response = await ai.models.generateContent({
                 model: "gemini-2.0-flash",
                 contents: msg,
@@ -41,6 +46,7 @@ export const Home = () => {
             )
             setAllMassages(allMassages)
             setIsResponsScreen(true)
+            setIsLoading(false)
             setQuestion("")
             // console.log(response.text);
         }
@@ -56,7 +62,7 @@ export const Home = () => {
                                 <h2 className='bg-[#181818] p-3 rounded-2xl  font-bold  text-[#ef476f]'>GeminiBot</h2>
                                 <button className='btn btn-soft  border-[#ef476f] text-[#ef476f]'>New Chat </button>
                             </div> */}
-                            <Navbar />
+                            
                             <div className="massages flex items-center justify-between w-[100vw] pt-[30px] px-[140px]">
                                 {
                                     allMassages.map((msg, index) => {
@@ -68,7 +74,6 @@ export const Home = () => {
                                     })
                                 }
                             </div>
-
                         </div> :
                         <div className="middle h-[80vh] flex items-center flex-col justify-center">
                             <h1 className='text-4xl'>GeminiBot</h1>
@@ -101,10 +106,15 @@ export const Home = () => {
                 }
                 <div className="bottom w-[100%]  flex flex-col items-center">
                     <div className="inputBox w-[70%] text-[15px] py-[7px] flex items-center bg-[rgb(24,24,24)] rounded-[30px]">
-                        <input type="text" value={question} onChange={(e) => { setQuestion(e.target.value) }} className='p-4 flex-1 bg-transparent outline-0 border-0 active:bg-white' placeholder='Write your message here...' id='searchBox' />
-                        {
+                        <input type="text" value={question} onChange={(e) => { setQuestion(e.target.value) }} className='p-4 flex-1 bg-transparent outline-0 border-0 :bg-white' placeholder='Write your message here...' id='searchBox' />
+                        {/* {
                             question == "" ? "" : <i onClick={handleRequest} className='text-[#F698AE] text-[25px] mr-6 cursor-pointer'><IoSend /></i>
-                        }
+                        } */}
+                        <button className='mr-3' disabled={isLoading}>
+                            {
+                                !isLoading ? <i onClick={handleRequest} className='text-[#F698AE] text-[25px] cursor-pointer'><IoSend /></i> : <span className="loading loading-spinner text-[#f58ca4]"></span>
+                            }
+                        </button>
                     </div>
                     <p className='mt-10 text-[gray] text-4'>GeminiBot is developed by <span className=' text-[#F698AE] hover:text-[#f58ca4]' loading="lazy"> <a target='_blank' href="https://mu-portfolio.web.app/">Muhammad Umar</a></span>&#128151;. by using the Gemini API for giving the response.</p>
                 </div >
